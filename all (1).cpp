f617894ffc10b5888fd700606f2801f452630e74
yashhhh <yashluthra80@gmail.com>
2026-07-10 01:44:04 +0530

    Add files via upload

---
+++ all (1).cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[1000];
+    int i;
+    for(i=0;i<n;i++){
+        cin >> a[i];
+    }
+int largest = a[0];
+for(i=0;i<n;i++){
+    if(a[i]>largest){
+        largest = a[i];
+    }
+}
+int secondlargest=a[1];
+for(i=0;i<n;i++){
+    if(a[i]!=largest && a[i]> secondlargest){
+        secondlargest=a[i];
+    }
+}
+cout << secondlargest;
+return 0;
+}
+
+
+
