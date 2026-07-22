7390952b6c61c502bca0bbbf335eca1de5d4d2fa
yashhhh <yashluthra80@gmail.com>
2026-07-23 00:37:22 +0530

    Add files via upload

---
+++ squareofarray.cpp
@@ -0,0 +1,21 @@
+#include<iosttreamm> 
+using namespace std;
+int main(){
+    int a[]={-4,2,3,10,5};
+    int n=sizeof(a)/sizeof(int);
+    int ans[100];
+    int i=0;
+    int j=n-1;
+    int k=n-1;
+    int index=0;
+    while(i<j){
+        if(abs(a[i])>abs(a[j]){
+
+        }
+            a[index]=a[j];
+            i--;
+            j++;
+        }
+    }
+
+}
