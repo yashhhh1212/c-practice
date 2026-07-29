000e5cda53cf78e7de324fd3670e364befe380d9
yashhhh <yashluthra80@gmail.com>
2026-07-29 21:44:59 +0530

    Add files via upload

---
+++ revision.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void largest(int arr[],int n){
+    int largest=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+          cout << largest;
+    cout << endl;
+}
+int main(){
+int a[1000];
+int n;
+cin >> n;
+int i;
+for( i=0;i<n;i++){
+    cin >> a[i];
+}
+largest(a,n);
+return 0;
+}
