ffa896783e057787e3f996a76bcede8067677e85
yashhhh <yashluthra80@gmail.com>
2026-08-17 01:57:41 +0530

    Add recursive function to sum numbers from 1 to n

---
+++ sumof1tonrecursive.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sum(int i,int n){
+    if(i>n){
+        return 0  ;
+    }
+    return i+sum(i+1,n);
+}
+int main(){
+    int n=11;
+    cout << sum(1,n);
+    return 0;
+}
