1e1a607449d4cb9939c9288dda8097b5e28156d4
yashhhh <yashluthra80@gmail.com>
2026-08-20 01:36:06 +0530

    Implement transpose for a 3x3 array
    Fix input and output loops for 3x3 array transpose.

---
+++ 3x32darrtranspose.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[3][3];
+    for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        cin >> a[i][j];
+    }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(j>i){
+                swap(a[i][j],a[j][i]);
+            }
+        }
+    }
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout << a[i][j]<< " ";
+}
+cout << endl;
+     }
+     return 0;
+    }
